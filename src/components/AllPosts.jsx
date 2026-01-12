d7e413bcb1d0ed243ea3efd7fe9cbe0ca44c680f
cmdrumright <cdrumright@protonmail.com>
2026-01-12 15:17:22 -0600

    Added topics service to get topics Added filter bar component with a topic dropdown filtered posts by topic id in AllPosts

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,9 +1,12 @@
 import {useEffect, useState} from "react"
 import {getAllPosts} from "../services/posts.jsx"
+import {FilterBar} from "./FilterBar.jsx"
 import "./Posts.css"
 
 export const AllPosts = () => {
     const [allPosts, setAllPosts] = useState([])
+    const [filterTopic, setFilterTopic] = useState(0)
+    const [filteredPosts, setFilteredPosts] = useState([])
 
     useEffect(() => {
         getAllPosts().then((postArray) => {
@@ -12,9 +15,15 @@ export const AllPosts = () => {
         })
     }, [])
 
-    return (
+    useEffect(() => {
+        const foundPosts = allPosts.filter((post) => filterTopic ? post.topic.id === filterTopic : true)
+        setFilteredPosts(foundPosts)
+    }, [filterTopic])
+
+    return (<>
+        <FilterBar setFilterTopic={setFilterTopic} />
         <div className="all-post-container">
-            {allPosts.map((post) => {
+            {filteredPosts.map((post) => {
                 return (
                     <div className="post-item" key={post.id}>
                         <h2>Title</h2>
@@ -27,6 +36,5 @@ export const AllPosts = () => {
                 )
             })}
         </div>
-    )
-
+    </>)
 }
---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react"
+import {getAllTopics} from "../services/topics.jsx"
+import {useEffect} from "react"
+
+export const FilterBar = ({setFilterTopic, setSearchTerm}) => {
+    const [allTopics, setAllTopics] = useState([])
+    //get list of topics
+    useEffect(() => {
+        getAllTopics().then((topics) => {
+            setAllTopics(topics)
+        })
+    }, [])
+
+    //setTopic if topic selected
+    const topicChange = (e) => {
+        setFilterTopic(+e.target.value)
+    }
+
+    //show topic selection box
+    return (
+        <div>
+            <label for="topic">Filter Topic</label>
+            <select name="topic" id="topic" onChange={topicChange}>
+                <option selected value={0} >All</option>
+            {allTopics.map((topic) => {
+                return (
+                    <option value={topic.id}>{topic.name}</option>
+                )
+            })}
+            </select>
+        </div>
+    )
+}
