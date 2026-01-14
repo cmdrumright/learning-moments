51e45c920028e7cf1166c2ccd4e8ece2452a04e7
cmdrumright <cdrumright@protonmail.com>
2026-01-14 14:13:12 -0600

    added currentUser state to ApplicationViews

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,8 +1,17 @@
 import {Outlet, Route, Routes} from "react-router-dom"
 import {NavBar} from "../components/nav/NavBar"
 import {AllPosts} from "../components/posts/AllPosts"
+import {useEffect, useState} from "react"
 
 export const ApplicationViews = () => {
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(() => {
+        const localLearningUser = localStorage.getItem("learning_user")
+        const learningUserObject = JSON.parse(localLearningUser)
+        setCurrentUser(learningUserObject)
+    }, [])
+
     return (
         <Routes>
             <Route
